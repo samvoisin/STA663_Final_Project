0c3091c497f9a2f2b1eccf8ede288a6eeb764ef5
Sam Voisin <psv6@duke.edu>
2019-04-24 19:57:58 -0400

    copying new bhc into sambranch

---
+++ bhc/__init__.py
@@ -0,0 +1,9 @@
+###############################################################################
+###################### Bayesian Hierarchical Clustering #######################
+###############################################################################
+
+
+from .hierarchytree import HierarchyTree
+from .leaf import Leaf
+from .split import Split
+from .helpfncs.hypothesis import *
