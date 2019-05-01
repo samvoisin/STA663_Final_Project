9cd4f5baeb5eccbc661a20e82f3900c6ef57322f
Sam Voisin <psv6@duke.edu>
2019-04-30 23:59:31 -0400

    setup updates

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 ### setup file for python bhc module ###
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
-setuptools.setup(
+with open("LICENSE") as lic:
+	license = lic.read()
+
+setup(
 	name = "bhc",
 	version = "0.0.1",
 	author = "Sam Voisin & Jonathan Klus",
@@ -13,8 +16,9 @@ setuptools.setup(
 	description = "This package implements the bayesian hierarchical clustering algorithm described by K. Heller and Z. Ghahramani (2005)",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
+	license = license,
 	url = "https://github.com/samvoisin/STA663_Final_Project",
-	packages = setuptools.find_packages(),
+	packages = find_packages(),
 	classifiers = [
 		"Programming Language :: Python :: 3",
 		"Development Status :: 3 - Alpha",
